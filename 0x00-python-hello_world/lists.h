bbe780ab1277ec3cb41ec6b708d12c407ecef686
e-las <omonigho.research@gmail.com>
2023-11-27 20:49:23 +0100

    Updated

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,24 +1,25 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *struct listint_t - singly linked list
- *@k: integer
- *@next: pointer to the next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
+ * Description: singly linked list node structure
+ * 
  */
-typedef struct listint_t
+typedef struct listint_s
 {
-	int k;
-	struct listint_t *next;
-}
-listint_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int k);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
