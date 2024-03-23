3c6aca911e08cf7459d7b7ae8348a99220f4e712
e-las <omonigho.research@gmail.com>
2024-03-23 15:42:45 +0100

    Solution to task 0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Define a class Square """
+
+class Square:
+    """This is the class defination of Square """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defination of a class Square """
+
+class Square:
+    """This is a class defination of class """
+
+    def __init__(self, size):
+        """initialize a new Square.
+        Args:
+            Size (int): the size of the new square."""
+        self.__size = size
